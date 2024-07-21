cb37ff7b89c6eb1f4ed41461f99ebb05d2df88d9
sensuy1 <sultanbejsenbekov@gmail.com>
2024-07-21 19:24:02 +0600

    fixed my errors

--- src/shared/ui/targetBanner/TargetBanner.jsx
+++ src/shared/ui/targetBanner/TargetBanner.jsx
@@ -1,14 +1,15 @@
 import styles from './styles/TargetBanner.module.css';
 import headPhone_img from '../../assets/images/m2mMobile/banner/banner_headPhone.png';
 import { Button } from '../../ui/customButton/Button'
+import { Link } from 'react-router-dom';
 
-export const TargetBanner = ({title, text, btn}) => {
+export const TargetBanner = ({title, text, btn, link, handleOpenModal}) => {
   return (
     <section className={styles.BB}>
       <div className={styles.BB__items}>
         <h1>{title}</h1>
         <p>{text}</p>
-        <Button>{btn}</Button>
+        <Link to={link}><Button onCLick={handleOpenModal}>{btn}</Button></Link>
       </div>
       <img src={headPhone_img} alt="headPhone_img" />
     </section>
